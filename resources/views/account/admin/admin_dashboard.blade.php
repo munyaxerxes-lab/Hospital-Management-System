08e9b3fb1e58217323e30832b0e9a82488527d5a
Munya Zaziecs <munyaxerxes@gmail.com>
2026-08-20 14:40:39 +0100

    setting up sidebar and page routes

---
+++ resources/views/account/admin/admin_dashboard.blade.php
@@ -0,0 +1,37 @@
+@extends('admin_layout.index')
+@section('content')
+      <div class="container">
+
+
+         <div class="statistic-cards">
+            
+            <div class="cards">
+               <div class="first_card_elements">
+               <i class="ri-calendar-line"></i>
+               </div>
+               <div class="second_card_elements">
+                  <h2 class="integers">00</h2>
+                  <i class="ri-calendar-line"></i>
+               </div>
+            
+
+            </div>
+
+               
+         </div>
+
+         <div class="graphs">
+
+
+            <div class="linear_graph">
+               
+
+            </div>
+            <div class="circular_graph">
+            
+            </div>
+         </div>
+         
+
+
+@endsection
---
+++ resources/views/account/admin/lab_request.blade.php
@@ -0,0 +1,5 @@
+@extends('admin_layout.index')
+@section('content')
+   <h1>Lab Request</h1>
+   <p>this is the lab request page</p>
+@endsection
