b9947d8ab1ab05ac9bbefc6cc65f8f5a1bd0a120
Munya Zaziecs <munyaxerxes@gmail.com>
2026-08-25 17:10:50 +0100

    admin settings implementation

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -11,29 +11,11 @@ class AdminUserSeeder extends Seeder
 {
     public function run(): void
     {
-        // 1. Fetch the correct internal primary ID for the admin role safely
-        $adminRole = Role::where('name', 'admin')->firstOrFail();
+        
+        $adminRole = Role::where('name','admin')->firstOrFail();
 
-        // // 2. Inject your special management credentials into the users table
-        // User::updateOrCreate(
-        //     ['email' => 'admin@medilink.com'], // Checks if this email already exists
-        //     [
-        //         'name' => 'Hospital Administrator',
-        //         'password' => Hash::make('SecureAdminSecret2026!'), // Change to a strong password
-        //         'role_id' => $adminRole->id,
-        //         'email_verified_at' => now(), // Bypasses public registration OTP steps
-        //     ]);
-
-        // User::updateOrCreate(
-        //     ['email' => 'munyapanki@gmail.com'], // Checks if this email already exists
-        //     [
-        //         'name' => 'Hospital Administrator',
-        //         'password' => Hash::make('612munyapanki2026!'), // Change to a strong password
-        //         'role_id' => $adminRole->id,
-        //         'email_verified_at' => now(), // Bypasses public registration OTP steps
-        //     ]
-           
-        // );
+       
+      
         // database/seeders/AdminUserSeeder.php
         User::updateOrCreate(
             ['email' => 'admin@medilink.com'],
